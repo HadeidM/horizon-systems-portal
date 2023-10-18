6c110a92b32c3c6ef369e140def8a0ab51dbedb3
Carina <99614462+Carinaguo@users.noreply.github.com>
2023-10-18 00:44:06 -0500

    perform database migrations using EF

---
+++ WebPortal/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using WebPortal.Context;
+using WebPortal.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebPortal.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username && x.Password == userObj.Password);
+
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+
+            return Ok(new
+            {
+                MessageProcessingHandler = "Login Successful!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "Registration Successful!"
+            });
+        }
+    }
+}
+
