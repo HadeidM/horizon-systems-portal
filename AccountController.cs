dd27b82769b7c55f15f357ba6898303d89fa3d42
Tonglu Yang (Remi) <tyang328@wisc.edu>
2023-10-23 15:09:27 -0500

    Update AccountController.cs with forgot pws recovery

--- AccountController.cs
+++ AccountController.cs
@@ -1,12 +1,21 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models; // Replace YourNamespace with the appropriate namespace for your models
+using YourNamespace.ViewModels; // Replace YourNamespace with the appropriate namespace for your view models
+
 public class AccountController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IEmailSender _emailSender; // You'll need an email sending service
 
-    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IEmailSender emailSender)
     {
         _userManager = userManager;
         _signInManager = signInManager;
+        _emailSender = emailSender;
     }
 
     // GET: /Account/Login
@@ -69,7 +78,88 @@ public class AccountController : Controller
         return View(model);
     }
 
-    // ... rest of the controller
+    // GET: /Account/ForgotPassword
+    [AllowAnonymous]
+    public IActionResult ForgotPassword()
+    {
+        return View();
+    }
+
+    // POST: /Account/ForgotPassword
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+            {
+                // Don't reveal that the user does not exist or is not confirmed
+                return View("ForgotPasswordConfirmation");
+            }
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+
+            await _emailSender.SendEmailAsync(model.Email, "Reset Password",
+               $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+
+            return View("ForgotPasswordConfirmation");
+        }
+
+        // If we got this far, something failed, redisplay form
+        return View(model);
+    }
+
+    // GET: /Account/ResetPassword
+    [AllowAnonymous]
+    public IActionResult ResetPassword(string code = null)
+    {
+        if (code == null)
+        {
+            return BadRequest("A code must be supplied for password reset.");
+        }
+
+        var model = new ResetPasswordViewModel { Code = code };
+        return View(model);
+    }
+
+    // POST: /Account/ResetPassword
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                // Don't reveal that the user does not exist
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ResetPasswordConfirmation", "Account");
+            }
+
+            AddErrors(result);
+        }
+
+        // If we got this far, something failed, redisplay form
+        return View(model);
+    }
+
+    // GET: /Account/ResetPasswordConfirmation
+    [AllowAnonymous]
+    public IActionResult ResetPasswordConfirmation()
+    {
+        return View();
+    }
 
     private void AddErrors(IdentityResult result)
     {
@@ -79,7 +169,7 @@ public class AccountController : Controller
         }
     }
 
-    private ActionResult RedirectToLocal(string returnUrl)
+    private IActionResult RedirectToLocal(string returnUrl)
     {
         if (Url.IsLocalUrl(returnUrl))
         {
@@ -91,4 +181,3 @@ public class AccountController : Controller
         }
     }
 }
-
