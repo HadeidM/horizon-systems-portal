be170c5ee62ded70408d17e3633ec61384bb44ab
Tonglu Yang (Remi) <tyang328@wisc.edu>
2023-11-12 21:15:37 -0600

    Create UserPreferencesDto.cs

---
+++ webapi/Models/Dto/UserPreferencesDto.cs
@@ -0,0 +1,10 @@
+namespace webapi.Models.Dto
+{
+    public class UserPreferencesDto
+    {
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public string LogoUrl { get; set; }
+        public string MFAOption { get; set; }
+    }
+}
